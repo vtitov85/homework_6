f510dc71413e3241ab7fcfe8a4bc3e485505eec6
VLAD <vlad@VLADs-MacBook-Air.local>
2019-03-23 01:08:00 -0500

    initial

---
+++ config.py
@@ -0,0 +1,4 @@
+api_key_nyt = "pNn21LxbSIpXjCseujHnGSsQGW9vRynY"
+api_key_weather = "a2c3defa6faf84cccbd85a9a02d19684"
+api_key_maps = "AIzaSyD5_c5SNJcJOWd6pQN25X-RhR9kI-jogNE"
+census_key = "85ac64b6b5a9c0901b00329d1ef41f0c53ccfc98"
